581248d06f9c652d94d45dfd4f9e96b9c1a0bd21
Oksana Gauci <oksanagauci98@gmail.com>
2024-01-02 21:53:38 +0100

    Unit 1 complete.

--- Unit1/sc-bed-simplephpsite-starter/aboutmariadb.php
+++ Unit1/sc-bed-simplephpsite-starter/aboutmariadb.php
@@ -1,8 +1,8 @@
 <!doctype html>
 <html lang="en">
-
+<?php include 'includes/head.php' ?>
 <body>
-
+    <?php include 'includes/menu.php' ?>
     <div class="container">
         <h1>MariaDB</h1>
         <p>MariaDB is a community-developed, commercially supported fork of the MySQL relational database management system (RDBMS), 
