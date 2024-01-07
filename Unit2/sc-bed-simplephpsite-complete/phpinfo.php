33a47b432f09d1e6ae580d01171a75a329391761
Oksana Gauci <oksanagauci98@gmail.com>
2024-01-07 12:39:07 +0100

    Unit 2 complete.

---
+++ Unit2/sc-bed-simplephpsite-complete/phpinfo.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+
+<?php include 'includes/head.php' ?>
+
+<body>
+     <?php include 'includes/menu.php' ?>
+
+     <div class="container">
+        <?php phpinfo(); ?>
+
+</div>
+
+        <?php include 'includes/footer.php' ?>
+
+</body>
+
+</html>
